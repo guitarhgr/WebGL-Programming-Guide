37982c105b67a31be3e3c921a875f373b4e1c1d3
HZC <815547360@qq.com>
2018-07-24 09:09:52 +0800

    平移三角形

--- Chapter-3/3.4-TranslatedTriangle.js
+++ Chapter-3/3.4-TranslatedTriangle.js
@@ -51,11 +51,45 @@ function main() {
 
 function initVertexBuffer(gl) {
     // 创建类型化数组
+    var vertices = new Float32Array([
+        0.0,0.5,  -0.5,-0.5,  0.5,-0.5
+    ]);
     // 定义定点数量
+    var n = 3;
+
     // 1. 创建缓冲区
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
     // 2. 绑定缓冲区对象
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
     // 3. 将数据写入缓冲区对象
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
     // 4. 将缓冲区对象分配给一个attribute变量
     // 获取a_Position变量的存储位置
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
     // 5. 开启attribute变量
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
 }
+
+/**划重点
+ * 1. 因为Tx, Ty, Tz对于所有顶点来说是固定(一致)的，所以我们用uniform变量u_Translation来表示三角形的平移距离
+ * 2. GLSL ES中的赋值操作只能发生在相同的类型的变量之间
+ * 3. 因为a_Position和u_Translation变量都是vec4类型的，所以可以直接相加，两个矢量的对应分量同时相加.
+ * 4. 为什么gl.uniform4f()的最后一个参数为0？
+ *      因为平移后点坐标第4分量w1+w2必须是1.0(因为点的位置坐标平移后还是一个点位置坐标)，
+ *      而w1是1.0(它是平移前点坐标第4分量)，所以平移矢量的本身的第4分量w2只能是0.0
+ * */
