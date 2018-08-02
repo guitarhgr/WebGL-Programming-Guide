fd4c7a6adb27e29f791966f459557bf03581b2c9
HZC <815547360@qq.com>
2018-08-02 09:45:08 +0800

    矩阵的操作(设置透视投影 正交投影 缩放 平移)

--- libs/cuon-matrix-copy.js
+++ libs/cuon-matrix-copy.js
@@ -169,11 +169,322 @@ Matrix4.prototype.setInverseOf = function (other) {
     d = this.elements;
     inv = new Float32Array(16);
 
-    inv[0] =
+    inv[0]  =   s[5]*s[10]*s[15] - s[5] *s[11]*s[14] - s[9] *s[6]*s[15]
+              + s[9]*s[7] *s[14] + s[13]*s[6] *s[11] - s[13]*s[7]*s[10];
+    inv[4]  = - s[4]*s[10]*s[15] + s[4] *s[11]*s[14] + s[8] *s[6]*s[15]
+              - s[8]*s[7] *s[14] - s[12]*s[6] *s[11] + s[12]*s[7]*s[10];
+    inv[8]  =   s[4]*s[9] *s[15] - s[4] *s[11]*s[13] - s[8] *s[5]*s[15]
+              + s[8]*s[7] *s[13] + s[12]*s[5] *s[11] - s[12]*s[7]*s[9];
+    inv[12] = - s[4]*s[9] *s[14] + s[4] *s[10]*s[13] + s[8] *s[5]*s[14]
+              - s[8]*s[6] *s[13] - s[12]*s[5] *s[10] + s[12]*s[6]*s[9];
+
+    inv[1]  = - s[1]*s[10]*s[15] + s[1] *s[11]*s[14] + s[9] *s[2]*s[15]
+              - s[9]*s[3] *s[14] - s[13]*s[2] *s[11] + s[13]*s[3]*s[10];
+    inv[5]  =   s[0]*s[10]*s[15] - s[0] *s[11]*s[14] - s[8] *s[2]*s[15]
+              + s[8]*s[3] *s[14] + s[12]*s[2] *s[11] - s[12]*s[3]*s[10];
+    inv[9]  = - s[0]*s[9] *s[15] + s[0] *s[11]*s[13] + s[8] *s[1]*s[15]
+              - s[8]*s[3] *s[13] - s[12]*s[1] *s[11] + s[12]*s[3]*s[9];
+    inv[13] =   s[0]*s[9] *s[14] - s[0] *s[10]*s[13] - s[8] *s[1]*s[14]
+              + s[8]*s[2] *s[13] + s[12]*s[1] *s[10] - s[12]*s[2]*s[9];
+
+    inv[2]  =   s[1]*s[6]*s[15] - s[1] *s[7]*s[14] - s[5] *s[2]*s[15]
+              + s[5]*s[3]*s[14] + s[13]*s[2]*s[7]  - s[13]*s[3]*s[6];
+    inv[6]  = - s[0]*s[6]*s[15] + s[0] *s[7]*s[14] + s[4] *s[2]*s[15]
+              - s[4]*s[3]*s[14] - s[12]*s[2]*s[7]  + s[12]*s[3]*s[6];
+    inv[10] =   s[0]*s[5]*s[15] - s[0] *s[7]*s[13] - s[4] *s[1]*s[15]
+              + s[4]*s[3]*s[13] + s[12]*s[1]*s[7]  - s[12]*s[3]*s[5];
+    inv[14] = - s[0]*s[5]*s[14] + s[0] *s[6]*s[13] + s[4] *s[1]*s[14]
+              - s[4]*s[2]*s[13] - s[12]*s[1]*s[6]  + s[12]*s[2]*s[5];
+
+    inv[3]  = - s[1]*s[6]*s[11] + s[1]*s[7]*s[10] + s[5]*s[2]*s[11]
+              - s[5]*s[3]*s[10] - s[9]*s[2]*s[7]  + s[9]*s[3]*s[6];
+    inv[7]  =   s[0]*s[6]*s[11] - s[0]*s[7]*s[10] - s[4]*s[2]*s[11]
+              + s[4]*s[3]*s[10] + s[8]*s[2]*s[7]  - s[8]*s[3]*s[6];
+    inv[11] = - s[0]*s[5]*s[11] + s[0]*s[7]*s[9]  + s[4]*s[1]*s[11]
+              - s[4]*s[3]*s[9]  - s[8]*s[1]*s[7]  + s[8]*s[3]*s[5];
+    inv[15] =   s[0]*s[5]*s[10] - s[0]*s[6]*s[9]  - s[4]*s[1]*s[10]
+              + s[4]*s[2]*s[9]  + s[8]*s[1]*s[6]  - s[8]*s[2]*s[5];
+
+    det = s[0]*inv[0] + s[1]*inv[4] + s[2]*inv[8] + s[3]*inv[12];
+    if (det === 0) {
+        return this;
+    }
+
+    det = 1 / det;
+    for (i = 0; i < 16; i++) {
+        d[i] = inv[i] * det;
+    }
 
     return this;
 };
 
+/**
+ * Calculate the inverse matrix of this, and set to this.
+ * @returns {Matrix4|this}
+ */
+Matrix4.prototype.invert = function () {
+    return this.setInverseOf(this);
+};
+
+/**
+ * Set the orthographic projection matrix.(设置正交投影的矩阵)
+ * @param left The coordinate of the left of clipping plane.
+ * @param right The coordinate of the right of clipping plane.
+ * @param bottom The coordinate of the bottom of clipping plane.
+ * @param top The coordinate of the top clipping plane
+ * @param near The distance to the nearer depth clipping plane. This value is minus if the plane is to be behind the viewer
+ * @param far The distances to the farther depth clipping plane. This value is minus if plane is to be behind the viewer.
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.setOrtho = function (left, right, bottom, top, near, far) {
+    var e, rw, rh, rd;
+
+    if (left === right || bottom === top || near === far) {
+        throw 'null frustum';
+    }
+
+    rw = 1 / (right - left);
+    rh = 1 / (top - bottom);
+    rd = 1 / (far - near);
+
+    e = this.elements;
+
+    e[ 0] = 2 * rw;
+    e[ 1] = 0;
+    e[ 2] = 0;
+    e[ 3] = 0;
+
+    e[ 4] = 0;
+    e[ 5] = 2 * rh;
+    e[ 6] = 0;
+    e[ 7] = 0;
+
+    e[ 8] = 0;
+    e[ 9] = 0;
+    e[10] = -2 * rd;
+    e[11] = 0;
+
+    e[12] = -(right + left) * rw;
+    e[13] = -(top + bottom) * rh;
+    e[14] = -(far + near) * rd;
+    e[15] = 1;
+
+    return this;
+};
+
+/**
+ * Multiply the orthographic projection matrix from the right.(右乘正交投影矩阵)
+ * @param left The coordinate of the left of clipping plane.
+ * @param right The coordinate of the right of clipping plane.
+ * @param bottom The coordinate of the bottom of clipping plane.
+ * @param top The coordinate of the top clipping plane
+ * @param near The distance to the nearer depth clipping plane. This value is minus if the plane is to be behind the viewer
+ * @param far The distances to the farther depth clipping plane. This value is minus if plane is to be behind the viewer.
+ * @returns {this}
+ */
+Matrix4.prototype.ortho = function (left, right, bottom, top, near, far) {
+    return this.concat(new Matrix4().setOrtho(left, right, bottom, top, near, far));
+};
+
+
+/**
+ * Set th perspective projection matrix. (设置透视投影矩阵)
+ * @param left The coordinate of the left of clipping plane.
+ * @param right The coordinate of the right of clipping plane.
+ * @param bottom The coordinate of the bottom of clipping plane.
+ * @param top The coordinate of the top clipping plane
+ * @param near The distance to the nearer depth clipping plane. This value is minus if the plane is to be behind the viewer
+ * @param far The distances to the farther depth clipping plane. This value is minus if plane is to be behind the viewer.
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.setFrustum = function (left, right, bottom, top, near, far) {
+    var e, rw, rh, rd;
+
+    if (left === right || top === bottom || near === far) {
+        throw 'null frustum';
+    }
+    if (near <= 0) {
+        throw 'near <= 0';
+    }
+    if (far <= 0) {
+        throw 'far <= 0';
+    }
+
+    rw = 1 / (right - left);
+    rh = 1 / (top - bottom);
+    rd = 1 / (far - near);
+
+    e = this.elements;
+
+    e[ 0] = 2 * near * rw;
+    e[ 1] = 0;
+    e[ 2] = 0;
+    e[ 3] = 0;
+
+    e[ 4] = 0;
+    e[ 5] = 2 * near * rh;
+    e[ 6] = 0;
+    e[ 7] = 0;
+
+    e[ 8] = (right + left) * rw;
+    e[ 9] = (top + bottom) * rh;
+    e[10] = -(far + near) * rd;
+    e[11] = -1;
+
+    e[12] = 0;
+    e[13] = 0;
+    e[14] = -2 * near * far * rd;
+    e[15] = 0;
+
+    return this;
+};
+
+/**
+ * Multiple the perspective projection matrix from the right.(右乘透视投影矩阵)
+ * @param left The coordinate of the left of clipping plane.
+ * @param right The coordinate of the right of clipping plane.
+ * @param bottom The coordinate of the bottom of clipping plane.
+ * @param top The coordinate of the top clipping plane
+ * @param near The distance to the nearer depth clipping plane. This value is minus if the plane is to be behind the viewer
+ * @param far The distances to the farther depth clipping plane. This value is minus if plane is to be behind the viewer.
+ * @returns {this}
+ */
+Matrix4.prototype.frustum = function (left, right, bottom, top, near, far) {
+    return this.concat(new Matrix4().setFrustum(left, right, bottom, top, near, far));
+};
+
+/**
+ * Set the perspective projection matrix by fovy and aspect.(通过视角和屏幕的宽高比来设置透视投影矩阵)
+ * @param fovy The angle between the upper and lower sides of the frustum.
+ * @param aspect The aspect ratio of the frustum.(width/height)
+ * @param near The distances to the nearer depth clipping plane. This value must be plus value.
+ * @param far The distance to farther depth clipping plane. This value must be plus value.
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.setPerspective = function (fovy, aspect, near, far) {
+    var e, rd, s, ct;
+
+    if (near === far || aspect === 0) {
+        throw 'null  frustum';
+    }
+    if (near <= 0) {
+        throw  'near <= 0';
+    }
+    if (far <= 0) {
+        throw 'far <= 0';
+    }
+
+    fovy = (Math.PI / 180) * fovy / 2;
+    s = Math.sin(fovy);
+
+    if (s === 0) {
+        throw 'null frustum';
+    }
+
+    rd = 1 / (far - near);
+    ct = Math.cos(fovy) / s;
+
+    e = this.elements;
+
+    e[ 0] = ct / aspect;
+    e[ 1] = 0;
+    e[ 2] = 0;
+    e[ 3] = 0;
+
+    e[ 4] = 0;
+    e[ 5] = 0;
+    e[ 6] = 0;
+    e[ 7] = 0;
+
+    e[ 8] = 0;
+    e[ 9] = 0;
+    e[10] = -(far + near) * rd;
+    e[11] = -1;
+
+    e[12] = 0;
+    e[13] = 0;
+    e[14] = -2 * near * far * rd;
+    e[15] = 0;
+
+    return this;
+};
+
+/**
+ * Multiply the perspective projection matrix from the right.(通过视角和屏幕的宽高比来设置透视投影矩阵)
+ * @param fovy The angle between the upper and lower sides of the frustum.
+ * @param aspect The aspect ratio of the frustum. (width/height)
+ * @param near The distances to the nearer depth clipping plane. This value must be plus value.
+ * @param far The distances to the farther depth clipping plane. This value must be plus value.
+ */
+Matrix4.prototype.perspective = function (fovy, aspect, near, far) {
+    return this.concat(new Matrix4().setPerspective(fovy, aspect, near, far));
+};
+
+/**
+ * Set the matrix for scaling.(设置矩阵缩放)
+ * @param x The scale factor along the X axis
+ * @param y The scale factor along the Y axis
+ * @param z The scale factor along the Z axis
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.setScale = function (x, y, z) {
+    var e = this.elements;
+    e[0] = x;   e[4] = 0;   e[ 8] = 0;  e[12] = 0;
+    e[1] = 0;   e[5] = y;   e[ 9] = 0;  e[13] = 0;
+    e[2] = 0;   e[6] = 0;   e[10] = z;  e[14] = 0;
+    e[3] = 0;   e[7] = 0;   e[11] = 0;  e[15] = 1;
+    return this;
+};
+
+/**
+ * Multiple the matrix for scaling from the right.(通过右乘矩阵来缩放)
+ * @param x The scale factor along the X axis
+ * @param y The scale factor along the Y axis
+ * @param z The scale factor along the Z axis
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.scale = function (x, y, z) {
+    var e = this.elements;
+    e[0] *= x;  e[4] *= y;  e[ 8] *= z;
+    e[1] *= x;  e[5] *= y;  e[ 9] *= z;
+    e[2] *= x;  e[6] *= y;  e[10] *= z;
+    e[3] *= x;  e[6] *= y;  e[11] *= z;
+    return this;
+};
+
+/**
+ * Set the matrix for translation(设置矩阵平移)
+ * @param x The X value of a translation
+ * @param y The Y value of a translation
+ * @param z The Z value of a translation
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.setTranslate = function (x, y, z) {
+    var e = this.elements;
+    e[0] = 1;   e[4] = 0;   e[ 8] = 0;  e[12] = x;
+    e[1] = 0;   e[5] = 1;   e[ 9] = 0;  e[13] = y;
+    e[2] = 0;   e[6] = 0;   e[10] = 1;  e[14] = z;
+    e[3] = 0;   e[7] = 0;   e[11] = 0;  e[15] = 1;
+    return this;
+};
+
+/**
+ * Multiple the matrix for translation from the right(右乘矩阵来平移)
+ * @param x The X value of a translation
+ * @param y The Y value of a translation
+ * @param z The Z value of a translation
+ * @returns {Matrix4}
+ */
+Matrix4.prototype.translate = function (x, y, z) {
+    var e = this.elements;
+
+    e[12] += e[0] * x + e[4] * y + e[ 8] * z;
+    e[13] += e[1] * x + e[5] * y + e[ 9] * z;
+    e[14] += e[2] * x + e[6] * y + e[10] * z;
+    e[15] += e[3] * x + e[7] * y + e[11] * z;
+
+    return this;
+};
 
 
 /**
